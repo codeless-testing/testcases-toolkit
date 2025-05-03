5aababff83775c013b9c4a22d165e1a9f2074c04
Vladislav Razyhraiev <nicorvenius@gmail.com>
2025-05-03 18:34:07 +0300

    some improvements

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+(() => {
+  let last;
+  document.addEventListener('mouseover', e => {
+    if (last) last.style.outline = '';
+    last = e.target;
+    last.style.outline = '2px solid rgba(255,0,0,.6)';
+  }, true);
+
+  document.addEventListener('click', e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const el = e.target;
+    const detail = {
+      tag: el.tagName,
+      id: el.id,
+      classes: el.className,
+      outerHTML: el.outerHTML
+    };
+    document.dispatchEvent(new CustomEvent('element-selected', { detail }));
+  }, true);
+})();
